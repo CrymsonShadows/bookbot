4f0ffcb959eda0f497df67936590da61fb77b9d4
Rahman Khandakar <khandakarrahman77@gmail.com>
2024-02-07 12:25:34 -0600

    Add letter counting for books

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ def main():
     text = get_text_from_book("books/frankenstein.txt")
     num_words = get_num_words(text)
     # print(file_contents)
-    print(f"The number of words in frankenstein is {num_words}!")
+    # print(f"The number of words in frankenstein is {num_words}!")
+    print(get_letter_count(text))
 
 def get_text_from_book(book_path):
     with open(book_path) as f:
@@ -11,5 +12,16 @@ def get_text_from_book(book_path):
 def get_num_words(text):
     words = text.split()
     return len(words)
+
+def get_letter_count(text):
+    letters = {}
+    for char in range(len(text)):
+        letter = text[char].lower()
+        if letter.isalpha():
+            if letter in letters:
+                letters[letter] += 1
+            else:
+                letters[letter] = 1
+    return letters
 
 main()

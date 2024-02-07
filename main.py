124f147a0eceb4a63a3c9bd61d44a55812557fd2
Rahman Khandakar <khandakarrahman77@gmail.com>
2024-02-07 12:32:56 -0600

    Change for loop in get_char_dict to for each

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ def main():
     num_words = get_num_words(text)
     # print(file_contents)
     # print(f"The number of words in frankenstein is {num_words}!")
-    print(get_letter_count(text))
+    print(get_char_dict(text))
 
 def get_text_from_book(book_path):
     with open(book_path) as f:
@@ -13,15 +13,14 @@ def get_num_words(text):
     words = text.split()
     return len(words)
 
-def get_letter_count(text):
+def get_char_dict(text):
     letters = {}
-    for char in range(len(text)):
-        letter = text[char].lower()
-        if letter.isalpha():
-            if letter in letters:
-                letters[letter] += 1
-            else:
-                letters[letter] = 1
+    for char in text:
+        lowered = char.lower()
+        if lowered in letters:
+            letters[lowered] += 1
+        else:
+            letters[lowered] = 1
     return letters
 
 main()

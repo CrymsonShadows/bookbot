f25838d85f99f4f3b8bb0cf4a7ae37cae1075233
Rahman Khandakar <khandakarrahman77@gmail.com>
2024-02-07 19:22:00 -0600

    Make book report of the number of words and occurrences of each letter

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 def main():
-    text = get_text_from_book("books/frankenstein.txt")
+    path = "books/frankenstein.txt"
+    text = get_text_from_book(path)
     num_words = get_num_words(text)
-    # print(file_contents)
-    # print(f"The number of words in frankenstein is {num_words}!")
-    print(get_char_dict(text))
+    char_dict = get_char_dict(text)
+    letter_list = get_letter_list(char_dict)
+    letter_list.sort(reverse=True, key=sort_on)
+    print(f"--- Begin report of {path} ---")
+    print(f"{num_words} words found in the document")
+    print("")
+    for letter in letter_list:
+        print(f"The '{letter["letter"]}' character was found {letter["num"]} times")
+    print("--- End report ---")
 
 def get_text_from_book(book_path):
     with open(book_path) as f:
@@ -22,5 +29,15 @@ def get_char_dict(text):
         else:
             letters[lowered] = 1
     return letters
+
+def get_letter_list(dict):
+    letters = []
+    for char in dict:
+        if char.isalpha():
+            letters.append({"letter": char, "num": dict[char]})
+    return letters
+
+def sort_on(dict):
+    return dict["num"]
 
 main()
